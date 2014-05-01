ed9ea5fed72dfd3e4cb6a2335b84f433f210dab1
Kyle Corbitt <kyle@corbt.com>
2014-05-01 17:42:36 -0600

    mostly working popups with month

--- map_points.py
+++ map_points.py
@@ -1,23 +1,25 @@
 import os
 import folium, vincent
 import pandas as pd
-import calendar
+import calendar,datetime
 
 def popup_chart(station):
-    mins = list(station[[str(num+1)+'_tmin' for num in xrange(12)]])
-    maxes = list(station[[str(num+1)+'_tmax' for num in xrange(12)]])
-    avgs = list(station[[str(num+1)+'_tavg' for num in xrange(12)]])
-    
-    months = [calendar.month_name[i+1] for i in range(12)]
-    
-    test = pd.DataFrame({"Min":mins, "Max":maxes, "Average":avgs})
-    chart = vincent.Line(test)
-    chart.axis_titles(x='Month',y="Temp (C)")
-    chart.legend(title="Temperature")
+    months = [i+1 for i in xrange(12)]
+    mins = list(station[[str(num)+'_tmin' for num in months]])
+    maxes = list(station[[str(num)+'_tmax' for num in months]])
+    avgs = list(station[[str(num)+'_tavg' for num in months]])
+
+    weather_frame = pd.DataFrame({"Min":mins, "Max":maxes, "Average":avgs, "month": months}).set_index("month")
+
+    chart = vincent.Line(weather_frame)
+    chart.axis_titles(x=station['name']+", "+station["country"],y="Temp (C)")
+    chart.legend(title="Key")
     chart.width=400
     chart.height=200
-    chart.to_json("viz/data_{0}.json".format(station['id']))
     chart.title=station['name']+", "+station["country"]
+    chart.padding={'top':20,'right':80,'bottom':40,'left':40}
+
+    chart.to_json("viz/data_{0}.json".format(station['id']))
     return chart
 
 f_map = folium.Map(
@@ -30,21 +32,12 @@ data = pd.read_csv("data/csv/categories.csv")
 cat_colors=['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a']
 
 for category,color in zip(data['category'].unique(),cat_colors):
-    # for _,station in data[data.category == category].iterrows():
-    for _,station in data[data.category == category].head(5).iterrows():
-        vis = vincent.Bar([10, 20, 30, 40, 30, 20])
-        # vis.tabular_data()
-        vis.to_json('viz/vis.json')
-        # map.polygon_marker(location=[45.5, -122.5], popup=(vis, 'vis.json'))
-
+    for _,station in data[data.category == category].iterrows():
         f_map.polygon_marker([station['lat'],station['long']], 
             radius=5, 
-            # popup=station['name']+", "+station["country"], 
             popup=(popup_chart(station),"data_{0}.json".format(station['id'])),
-            # popup = (vis, 'vis.json'),
             line_color=None,
             fill_color=color, 
             fill_opacity=1)
 
-os.remove('viz/map.html')
 f_map.create_map(path='viz/map.html')
