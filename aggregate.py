34f634e4a47e872215329823dc61ad68a79f44e2
Kyle Corbitt <kyle@corbt.com>
2014-05-01 17:42:19 -0600

    reverse the southern hemisphere in clustering so the reversal isn't written to disk

--- aggregate.py
+++ aggregate.py
@@ -17,18 +17,5 @@ data = data.groupby(by='id').mean().drop('year',axis=1)
 index = pd.read_csv("data/csv/tavg-inv.csv")
 data = pd.merge(data.reset_index(), index, on=['id']).set_index('id')
 
-# The following code reverses the year for the southern hemisphere.
-# This allows northern and southern climates to be compared
-cols = data.columns
-first_months = [col for col in cols if re.match('[1-6]_', col)]
-last_months = [col for col in cols if re.match('\d+_', col) and col not in first_months]
-
-south_data = data[data.lat < 0]
-first_months_data = south_data[first_months]
-south_data[first_months] = south_data[last_months]
-south_data[last_months] = first_months_data
-
-data.loc[south_data.index] = south_data
-
 # Write file to disk
 data.to_csv("data/csv/data.csv")
--- cluster.py
+++ cluster.py
@@ -1,20 +1,33 @@
 import numpy as np
 import pandas as pd
 import re
-
 from sklearn.cluster import MiniBatchKMeans
-# from sklearn import metrics
-# from sklearn.preprocessing import StandardScaler
+
+def reverse_south(dataset):
+    # The following code reverses the year for the southern hemisphere.
+    # This allows northern and southern climates to be compared
+    cols = dataset.columns
+    first_months = [col for col in cols if re.match('[1-6]_', col)]
+    last_months = [col for col in cols if re.match('\d+_', col) and col not in first_months]
+
+    south_data = dataset[dataset.lat < 0]
+    first_months_data = south_data[first_months]
+    south_data[first_months] = south_data[last_months]
+    south_data[last_months] = first_months_data
+
+    dataset.loc[south_data.index] = south_data
+    return dataset
 
 
 data = pd.read_csv("data/csv/data.csv").set_index('id')
 
-# print pd.isnull(data.drop(['lat','long','name','country'],axis=1)).any(1).nonzero()[0]
-
+data = reverse_south(data)
 prediction_data = data.drop(['lat','long','name','country'],axis=1).fillna(20)
 
 d = MiniBatchKMeans(n_clusters=10).fit(prediction_data)
 
 data['category'] = d.predict(prediction_data)
+
+data = reverse_south(data)
 
 data.to_csv("data/csv/categories.csv")
